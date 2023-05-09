860f142b8bc7e1eeafd286da949abeeb520bb83b
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2023-05-08 21:25:14 -0400

    Login Screen

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/services/auth.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -6,7 +7,13 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-    );
+        appBar: AppBar(
+          title: Text('Profile'),
+        ),
+        body: ElevatedButton(
+            child: Text('Sign Out'),
+            onPressed: () async {
+              await AuthService().signOut;
+            }));
   }
 }
