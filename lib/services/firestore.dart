b83aa50735b53f52815d307466118e550c5cb54d
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2023-05-13 19:12:28 -0400

    write to database

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -11,16 +11,41 @@ class FirestoreService {
   Future<List<Topic>> getTopics() async {
     var ref = _db.collection('topics');
     var snapshot = await ref.get();
-    var data = snapshot.docs.map((s)=>s.data());
-    var topics = data.map.((d)=> Topic.fromJson(d));
+    var data = snapshot.docs.map((s) => s.data());
+    var topics = data.map((d) => Topic.fromJson(d));
     return topics.toList();
   }
 
 //basic data retrieval
-Future<Quiz> getQuiz(String quizId) async {
-  var ref= _db.collection('quizzes').doc(quizId);
-  var snapshot = await ref.get();
-  return Quiz.fromJson(snapshot.data() ?? {});
-}
+  Future<Quiz> getQuiz(String quizId) async {
+    var ref = _db.collection('quizzes').doc(quizId);
+    var snapshot = await ref.get();
+    return Quiz.fromJson(snapshot.data() ?? {});
+  }
 
+// Listens to current user's report document in Firestore
+  Stream<Report> streamReport() {
+    return AuthService().userStream.switchMap((user) {
+      if (user != null) {
+        var ref = _db.collection('reports').doc(user.uid);
+        return ref.snapshots().map((doc) => Report.fromJson(doc.data()!));
+      } else {
+        return Stream.fromIterable([Report()]);
+      }
+    });
+  }
+
+//update report document
+  Future<void> updateUserReport(Quiz quiz) {
+    var user = AuthService().user!;
+    var ref = _db.collection('reports').doc(user.uid);
+
+    var data = {
+      'total': FieldValue.increment(1);
+      'topics': {
+        quiz.topic: FieldValue.arrayUnion([quiz.id])
+      }
+    };
+    return ref.set(data, SetOptions(merge: true));
+  }
 }
