f67a8eac728d41d307dcbc9ba3328226ca4ad10b
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2023-05-26 02:10:51 -0400

    quiz ui

--- lib/quiz/quiz.dart
+++ lib/quiz/quiz.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:quiz_app/quiz/quiz_state.dart';
+import 'package:quiz_app/services/firestore.dart';
+import 'package:quiz_app/shared/progress_bar.dart';
+
+import '../services/models.dart';
 
 class QuizScreen extends StatelessWidget {
-  const QuizScreen({super.key});
-
+  const QuizScreen({Key? key, required this.quizId}) : super(key: key);
+  final String quizId;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ChangeNotifierProvider(
+      create: (_) => QuizState(),
+      child: FutureBuilder<Quiz>(
+        future: FirestoreService().getQuiz(quizId),
+        builder: (context, snapshot) {
+          var state = Provider.of<QuizState>(context);
+
+          if (!snapshot.hasData || snapshot.hasError) {
+            return Loader();
+          } else {
+            var quiz = snapshot.data!;
+
+            return Scaffold(
+              appBar: AppBar(
+                title: AnimatedProgressBar(value: state.progress),
+                leading: IconButton(
+                  icon: const Icon(FontAwesomeIcons.xmark),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
